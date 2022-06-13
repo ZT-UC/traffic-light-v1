428484f2b022837d3352f813362fa1d55e05bde3
ZT-UC <droip334@gmail.com>
2022-06-13 23:13:21 +0200

    Initial Commit

---
+++ traffic-light-v1/traffic_light_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  traffic_light_v1App.swift
+//  traffic-light-v1
+//
+//  Created by Dominik Ď. on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct traffic_light_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
